dd69cbc26ca1f1709f3fcbd2dd1cfa8a1ab179ad
Tarek_Nady <34241000+Tarek-Nady@users.noreply.github.com>
2024-01-15 20:42:42 +0200

    Update PurchaseResponse.java

--- backend/src/main/java/com/tarek/ecommerceapp/dto/PurchaseResponse.java
+++ backend/src/main/java/com/tarek/ecommerceapp/dto/PurchaseResponse.java
@@ -5,5 +5,5 @@ import lombok.Data;
 
 @Data
 public class PurchaseResponse {
-    private final String orderTrackingNumber;
+    private final String orderTrackingNumber; //response with tracking number after you valid purchase
 }
