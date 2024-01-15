060add4d1f033916b4b8eec5fa2ccd13595edaa2
Tarek_Nady <34241000+Tarek-Nady@users.noreply.github.com>
2024-01-15 20:41:48 +0200

    Update CheckoutService.java

--- backend/src/main/java/com/tarek/ecommerceapp/service/CheckoutService.java
+++ backend/src/main/java/com/tarek/ecommerceapp/service/CheckoutService.java
@@ -1,11 +1,19 @@
+// Package declaration
 package com.tarek.ecommerceapp.service;
 
+// Import statements
 import com.stripe.model.PaymentIntent;
 import com.tarek.ecommerceapp.dto.Purchase;
 import com.tarek.ecommerceapp.dto.PurchaseResponse;
 import com.tarek.ecommerceapp.dto.PaymentInfo;
 import com.stripe.exception.StripeException;
+
+// Public interface declaration
 public interface CheckoutService {
+
+    // Method signature for placing an order
     PurchaseResponse placeOrder(Purchase purchase);
+
+    // Method signature for creating a payment intent with Stripe
     PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
