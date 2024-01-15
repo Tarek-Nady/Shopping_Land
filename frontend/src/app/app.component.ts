4f16af7a74f0e77d628b7ad6dc14887f1ef640ce
Tarek_Nady <34241000+Tarek-Nady@users.noreply.github.com>
2024-01-15 20:14:51 +0200

    Update app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
+// Importing the Component decorator from Angular core
 import { Component } from '@angular/core';
 
+// The @Component decorator function is used to define metadata about the component
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // The CSS selector for the component; used in HTML to instantiate this component
+  templateUrl: './app.component.html', // The path to the HTML template file for this component
+  styleUrls: ['./app.component.css'] // The path to the CSS styles for this component
 })
 export class AppComponent {
+  // A property 'title' defined on the AppComponent class
   title = 'angular-ecommerce';
 }
